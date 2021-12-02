7165a5441ed29e002043c63954c996f74832f30c
Lynn Nguyen <AmandaN22@outlook.com>
2021-12-01 18:09:18 -0700

    Add test for new instance of Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect; 
 
 const Turn = require('../src/Turn'); 
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
@@ -9,4 +10,43 @@ describe('Turn', function() {
     const turn = new Turn(); 
     expect(Turn).to.be.a('function');
   });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+  it('should have a users guess to the question of a Card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    expect(turn.userAnswer).to.equal('array');
+  });
+
+  it('should be able to return the guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+
+    turn.returnGuess(); 
+    expect(turn.userAnswer).to.equal('array')
+  });
+
+  it('should be able to return the card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+
+  });
+
+  it('should indicate if the users guess matches the correct answer on the card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    const evaluateGuess = turn.evaluateGuess(); 
+
+  }): 
+
+  it('should be able to give feedback based on whether the guess is correct or not', function () {
+    const turn = new Turn(); 
+    const giveFeedback = turn.giveFeedback(); 
+
+  });
+
 });
