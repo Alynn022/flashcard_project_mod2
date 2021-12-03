34038f01817c1ab2bcbb411a8bab84df65f66400
Lynn Nguyen <AmandaN22@outlook.com>
2021-12-02 19:37:35 -0700

    refactor code comments and run linter

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect; 
 
-const Turn = require('../src/turn'); 
-const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
@@ -10,7 +8,6 @@ const Game = require('../src/Game');
 describe('Game', function() {
 
   it('should be a function', function() {
-    const game = new Game(); 
     expect(Game).to.be.a('function'); 
   })
  
@@ -20,18 +17,10 @@ describe('Game', function() {
   });
 
   it('should keep track of the current round', function() {
-    const turn = new Turn();
     const deck = new Deck();
     const round = new Round(deck)
     const game = new Game(round);
 
     expect(game.currentRound).to.equal(round)
-  })
-  it('should be able to start the game', function() {
-    // const game = new Game();
-    
-    // let newGame = game.start(); 
-    // expect(newGame).to.be.an.instanceof(Card) 
-   
   })
 })
